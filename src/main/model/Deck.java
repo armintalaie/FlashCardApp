bcaf31e748153ff73e64c99b5f494510719a8ca0
armintalaie <armin.talaie@alumni.ubc.ca>
2020-02-11 01:07:55 -0800

    added methods to Deck and DeckTest

--- src/main/model/Deck.java
+++ src/main/model/Deck.java
@@ -1,4 +1,38 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Deck {
+
+    public Deck(String name) {
+        //stub
+    }
+
+    public String getName() {
+        //stub
+        return null;
+    }
+
+    public void setName(String name) {
+        //stub
+    }
+
+    public void addCard(FlashCard card) {
+        //stub
+    }
+
+    public boolean removeCard(FlashCard card) {
+        //stub
+        return false;
+    }
+
+    public ArrayList<FlashCard> getCards() {
+        //stub
+        return null;
+    }
+
+    public int numberOfCards() {
+        //stub
+        return 0;
+    }
 }
--- src/test/model/DeckTest.java
+++ src/test/model/DeckTest.java
@@ -1,4 +1,76 @@
 package model;
 
-public class DeckTest {
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+    private Deck deck;
+    private FlashCard flashCard;
+    private FlashCard flashCard1;
+
+    @BeforeEach
+    void runBefore() {
+        deck = new Deck("Math");
+        flashCard = new FlashCard("2 + 2 = ?", "4");
+        flashCard1 = new FlashCard("2 x 3 = ?", "6");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals(deck.getName(), "Math");
+        assertEquals(deck.numberOfCards(), 0);
+    }
+
+    @Test
+    void testSetName() {
+        deck.setName("Math101");
+        assertEquals(deck.getName(), "Math101");
+    }
+
+    @Test
+    void testAddCard() {
+        assertEquals(deck.numberOfCards(), 0);
+        FlashCard flashCard = new FlashCard("2 + 2 = ?", "4");
+        deck.addCard(flashCard);
+        assertEquals(deck.numberOfCards(), 1);
+        assertEquals(deck.getCards().get(0), flashCard);
+    }
+
+    @Test
+    void testMultipleAddCard() {
+        assertEquals(deck.numberOfCards(), 0);
+
+        deck.addCard(flashCard);
+        assertEquals(deck.numberOfCards(), 1);
+
+        deck.addCard(flashCard1);
+        assertEquals(deck.numberOfCards(), 2);
+
+        ArrayList<FlashCard> list = new ArrayList<FlashCard>();
+        list.add(flashCard);
+        list.add(flashCard1);
+
+        assertEquals(deck.getCards(), list);
+    }
+
+    @Test
+    void removeCard() {
+
+        deck.addCard(flashCard);
+        assertFalse(deck.removeCard(flashCard1));
+        assertEquals(deck.numberOfCards(), 1);
+
+        deck.addCard(flashCard1);
+        assertEquals(deck.numberOfCards(), 2);
+        assertTrue(deck.removeCard(flashCard1));
+        assertEquals(deck.numberOfCards(), 1);
+
+
+    }
 }
+
+
