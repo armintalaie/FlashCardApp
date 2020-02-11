498cd678211587ff37db6d1c935f76d98f1137b9
armintalaie <armin.talaie@alumni.ubc.ca>
2020-02-11 00:39:33 -0800

    added methods to FlashCard app

--- src/main/model/FlashCard.java
+++ src/main/model/FlashCard.java
@@ -1,4 +1,26 @@
 package model;
 
 public class FlashCard {
+
+    public FlashCard(String front, String back) {
+        //stub
+    }
+
+    public void setFront(String front) {
+        //stub
+    }
+
+    public void setBack(String back) {
+        //stub
+    }
+
+    public String getFront() {
+        //stub
+        return null;
+    }
+
+    public String getBack() {
+        //stub
+        return null;
+    }
 }
