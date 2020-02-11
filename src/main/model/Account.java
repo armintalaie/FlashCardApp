980e631ce43a8830e9f4e2f0e72c31792ea14bf7
armintalaie <armin.talaie@alumni.ubc.ca>
2020-02-11 10:58:30 -0800

    Task 3: added all classes and methods needed + proper documentation

--- src/main/model/Account.java
+++ src/main/model/Account.java
@@ -2,41 +2,66 @@ package model;
 
 import java.util.ArrayList;
 
+// Represents an account with name that stores decks and flashcards
 public class Account {
 
+
+    //Creates a Deck
+    //EFFECTS : sets Account's name and creates a deck's list with a default deck
     public Account(String name) {
         //stub
     }
 
+    //gets account's name
+    //EFFECTS : return's account's name
     public String getName() {
         //stub
         return null;
     }
 
+    //changes account's name
+    //MODIFIES: THIS
+    //EFFECTS : changes Account's name
     public void setName(String name) {
         //stub
     }
 
+    //makes a deck
+    //MODIFIES: THIS
+    //EFFECTS : gets a String and makes a deck and adds to list of decks if there isn't one with that name
+    // and returns true otherwise it will return false
     public boolean makeDeck(String name) {
         //stub
         return false;
     }
 
+    //adds a deck
+    //MODIFIES: THIS
+    //EFFECTS : gets a Deck adds to list of decks if there isn't one with that name and returns true
+    // otherwise it will return false
     public boolean addDeck(Deck deck) {
         //stub
         return false;
     }
 
+    //removes a deck
+    //MODIFIES: THIS
+    //EFFECTS : removes a Deck from list of decks if deck exists that name and returns true
+    // otherwise it will return false
     public boolean removeDeck(Deck deck) {
         //stub
         return false;
     }
 
+    //Gets list of decks
+    //EFFECTS : returns list of decks
     public ArrayList<Deck> getDecks() {
         //stub
         return null;
     }
 
+    //Gets number decks in account
+    //EFFECTS : return length of list of decks
     public int numberOfDecks() {
         //stub
         return 0;
--- src/main/model/Deck.java
+++ src/main/model/Deck.java
@@ -2,35 +2,54 @@ package model;
 
 import java.util.ArrayList;
 
+// Represents a deck with name that stores flashcards
 public class Deck {
 
+
+    //Creates a Deck
+    //EFFECTS : sets Deck's name and creates empty cards list
     public Deck(String name) {
         //stub
     }
 
+    //Gets a Deck's name
+    //EFFECTS : returns Deck's name
     public String getName() {
         //stub
         return null;
     }
 
+    //Changes the deck's name
+    //MODIFIES: THIS
+    //EFFECTS : sets Deck's name
     public void setName(String name) {
         //stub
     }
 
+    //Adds a card to deck
+    //MODIFIES: THIS
+    //EFFECTS : adds flashcard to list of cards
     public void addCard(FlashCard card) {
         //stub
     }
 
+    //Removes a card to deck
+    //MODIFIES: THIS
+    //EFFECTS : removes flashcard from list of cards
     public boolean removeCard(FlashCard card) {
         //stub
         return false;
     }
 
+    //Gets list of flashcards
+    //EFFECTS : returns list of cards
     public ArrayList<FlashCard> getCards() {
         //stub
         return null;
     }
 
+    //Gets number cards in deck
+    //EFFECTS : return length of list of cards
     public int numberOfCards() {
         //stub
         return 0;
